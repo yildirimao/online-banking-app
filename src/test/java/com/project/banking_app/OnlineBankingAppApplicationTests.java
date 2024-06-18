1cb93c9fcdb2ea3f3abb2ec2e0277e34e19117ae
yildirimao <yldrmao@gmail.com>
2024-06-18 16:19:53 -0400

    new commit

---
+++ src/test/java/com/project/banking_app/OnlineBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.banking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
